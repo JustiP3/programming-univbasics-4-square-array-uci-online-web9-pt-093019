28a3ce159bf05d67a12a3b3b39228d90825d5f86
Justin Phillips <justinphil02@yahoo.com>
2019-10-02 01:48:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,13 @@ counter = 0
 
 while (array) do 
   len = array.length 
+  if (len < (counter + 1))
+  return array 
+else 
   array [counter] = array[counter] * array[counter]
   counter += 1
+end 
+
 end
 
 
